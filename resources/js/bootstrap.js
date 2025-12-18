941dd66112730dd51358c9f5020e672cc01ff3fe
tashpopkaa-glitch <tashpopka.a@gmail.com>
2025-12-18 05:39:39 +0500

    Disable Pusher when VITE_PUSHER_APP_KEY is missing

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,13 +1,15 @@
-import axios from 'axios';
-window.axios = axios;
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+if (import.meta.env.VITE_PUSHER_APP_KEY) {
+    window.Pusher = Pusher;
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
-import './echo';
-
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+        forceTLS: true
+    });
+} else {
+    console.warn('Pusher disabled: no VITE_PUSHER_APP_KEY');
+}
