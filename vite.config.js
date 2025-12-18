354d7cf40e3a15334080523e67658b4b5afd29be
tashpopkaa-glitch <tashpopka.a@gmail.com>
2025-12-18 05:51:30 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    // 🔴 ENG MUHIM QATOR — CSS/JS yo‘li shu bilan to‘g‘rilanadi
+    base: '/build/',
+
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
+            input: ['resources/js/app.jsx'],
+            refresh: false, // production uchun
         }),
         react(),
     ],
